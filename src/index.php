7acebabb5b419a2433010bfd99315ad9d9d7dcb9
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-04-15 11:27:49 +0200

    update
    added a introduction place holder it will scale with the screen size

--- src/index.php
+++ src/index.php
@@ -14,9 +14,15 @@
     <?php include 'header.php';?>
 
     <body>
-        <div class="Home-page-container">
-            <div class="box"></div>
-            <div class="box"></div>
+        <div class="home-body-introduction-container">
+                <img class="home-body-introduction-image" src="assets/images/placeholder.png" alt="Cool gif">
+
+                <div class="title-text-contaniner">
+                    <h2 class="tile-text">Introduction</h2>
+                    <div class="home-body-introduction-text-container">
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    </div>
+                </div>
         </div>
     </body>
 
